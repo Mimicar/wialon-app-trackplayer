b661bf76c259cc5dd50a22a23d6d0d66a9ed047f
Mimicar <mimicar@mimicar.sk>
2014-03-17 11:56:23 +0100

    Create sk.js

---
+++ lang/lang/sk.js
@@ -0,0 +1,215 @@
+TRANSLATIONS = {
+	"Track player": "Prehrávanie trasy",
+
+	"Monitoring info": "",
+
+	"Interval": "",
+
+	"Today": "Dnes",
+
+	"Yesterday": "Včera",
+
+	"Current week": "Aktuálny týždeň",
+
+	"Current month": "Aktuálny mesiac",
+
+	"7 days": "7 dn",
+
+	"30 days": "30 dn",
+
+	"Custom interval": "Vlastný interval",
+
+	"Unit": "Jednotka",
+
+	"Change interval": "Zmeniť interval",
+
+	"Show track": "Zobraziť trasu",
+
+	"Tracks": "Trasy",
+
+	"Playback speed": "Rýchlosť prehrávania",
+
+	"Rotate icon": "Otáčať ikonu",
+
+	"Double-click on necessary sensors below to track them.": "Dvojklikom pridajte nižšie uvedené senzory, ktoré chcete sledovať",
+
+	"General information": "Všeobecné informácie",
+
+	"Hide settings": "Skryť nastavenie",
+
+	"Sensors": "Senzory",
+
+	"Skip intervals between trips": "Skryť intervaly medzi trasami",
+
+	"Prev": "Predchádzajúci",
+
+	"Next": "Ďaľší",
+
+	"Login error, restart the application": "Chyba prihlásenia, reštartujte aplikáciu",
+
+	"Invalid time interval": "Neplatný časový interval",
+
+	"Select unit": "Vyberte jednotku",
+
+	"There is a track for this unit already": "Trasa pre danú jednotku už existuje",
+
+	"No messages for the selected interval": "Žiadne správy pre zvolený interval",
+
+	"Unable to build a track": "Nie je možné vytvoriť trasu",
+
+	"Unable to load trips": "Nie je možné načítať trasy",
+
+	"Session error, restart the application": "Chyba, reštartujte aplikáciu",
+
+	"Init messages error": "Chyba správ",
+
+	"Nothing to delete, no tracks for the given unit": "Nemožno odstrániť, žiadne trasy pre danú jednotku",
+
+	"Hide/show track": "Skryť / zobraziť trasu",
+
+	"Focus on unit": "Zameranie na jednotku",
+
+	"Delete track": "",
+
+	"Settings": "",
+
+	"Follow the unit on map": "",
+
+	"Show/hide pictures": "",
+
+	"Show/hide parameters": "",
+
+	"Speed": "",
+
+	"mph": "",
+
+	"kph": "",
+
+	"miles": "",
+
+	"km": "",
+
+	"Go to the first message": "",
+
+	"Beginning": "",
+
+	"Go to the last message": "",
+
+	"End": "",
+
+	"Unable to load events": "",
+
+	"%d events have been found for %s. First 1000 of them will be shown.\nTo see other events, change time interval.": "",
+
+	"January": "",
+	"February": "",
+	"March": "",
+	"April": "",
+	"May": "",
+	"June": "",
+	"July": "",
+	"August": "",
+	"September": "",
+	"October": "",
+	"November": "",
+	"December": "",
+
+	"Jan": "",
+	"Feb": "",
+	"Mar": "",
+	"Apr": "",
+	"Jun": "",
+	"Jul": "",
+	"Aug": "",
+	"Sep": "",
+	"Oct": "",
+	"Nov": "",
+	"Dec": "",
+
+	"Sunday": "",
+	"Monday": "",
+	"Tuesday": "",
+	"Wednesday": "",
+	"Thursday": "",
+	"Friday": "",
+	"Saturday": "",
+
+	"Sun": "",
+	"Mon": "",
+	"Tue": "",
+	"Wed": "",
+	"Thu": "",
+	"Fri": "",
+	"Sat": "",
+
+	"Su": "",
+	"Mo": "",
+	"Tu": "",
+	"We": "",
+	"Th": "",
+	"Fr": "",
+	"Sa": "",
+
+	"Time": "",
+	"Hours": "",
+	"Minutes": "",
+	"Seconds": "",
+
+	//Sensors
+	"Absolute engine hours": "",
+	"Absolute fuel consumption sensor": "",
+	"Accelerometer": "",
+	"Counter sensor": "",
+	"Custom sensor": "",
+	"State sensor": "",
+	"Custom digital sensor": "",
+	"Driver binding": "",
+	"Engine efficiency sensor": "",
+	"Engine ignition sensor": "",
+	"Engine revs sensor": "",
+	"Equipment binding": "",
+	"Fuel level sensor": "",
+	"hours": "",
+	"Impulse fuel consumption sensor": "",
+	"Fuel level impulse sensor": "",
+	"Instant fuel consumption sensor": "",
+	"litres": "",
+	"m": "",
+	"Mileage sensor": "",
+	"Relative odometer": "",
+	"On/Off": "",
+	"Relative engine hours": "",
+	"rpm": "об/мин",
+	"Enter sensor name, type, description or parameter.": "",
+	"Temperature coefficient": "",
+	"Temperature sensor": "",
+	"Trailer binding": "",
+	"V": "",
+	"Voltage sensor": "",
+	"on": "",
+	"off": "",
+
+	//Messages
+	"GPRS traffic counter value: %d KB.": "",
+	"GPRS traffic counter reset. %d KB consumed.": "",
+	"Engine hours counter value is %d h.": "",
+	"Engine hours counter value was changed from %d h to %d h.": "",
+	"Mileage counter value is %d km.": "",
+	"Mileage counter value was changed from %d km to %d km.": "",
+	"Route '%s': round aborted.": "",
+	"Route '%s': arrival at point '%s'.": "",
+	"Route '%s': departure from point '%s'.": "",
+	"Route '%s': round finished.": "",
+	"Route '%s': unit returned to schedule.": "",
+	"Route '%s': point '%s' skipped.": "",
+	"Route '%s': round by schedule '%s' started.": "",
+	"Route '%s': unit is ahead of schedule.": "",
+	"Route '%s': unit is late.": "",
+	"Route %s: entered %s": "",
+	"Route %s: left %s": "",
+	"Route": "",
+	"Event": "",
+	"Filling": "",
+	"Maintenance": "",
+	"Violation": ""
+};
